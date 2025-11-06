a55cb3d8dcdad1fe9aea3b5d93b2f1c8853705eb
Abdullah Khan <abdullahkhan@Abdullahs-MacBook-Air.local>
2025-11-06 17:05:30 +0500

    test 10 : deployement

--- components/auth/Login.tsx
+++ components/auth/Login.tsx
@@ -63,7 +63,7 @@ export default function Login() {
     mutationFn: async (values: LoginFormValues) => {
       try {
         // Call backend first to get a precise error message
-        const baseURL = "http://54.183.140.154:3005/docs/api/v1/user/login";
+        const baseURL = "http://54.183.140.154:3005/api/v1/user/login";
         setdebugURL(process.env.API_BASE_URL ? process.env.API_BASE_URL : "null");
         const resp = await fetch(
           baseURL,
